807af09dd9fe2cf191a06944d58f943f926e62ed
robiul-hub <75095486+robiul-hub@users.noreply.github.com>
2021-01-28 17:57:21 +0000

    Update variable.tf

--- rds/variable.tf
+++ rds/variable.tf
@@ -3,3 +3,9 @@ variable "rds_subnet2" {}
 variable "db_instance" {}
 variable "vpc_id" {}
 variable "security_groups" {}
+variable "username" {
+  type = string
+  }
+variable "password" {
+  type = string
+  }
