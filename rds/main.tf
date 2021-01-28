337439ee03e33655deced5cbd7a4fbf56d265100
robiul-hub <75095486+robiul-hub@users.noreply.github.com>
2021-01-28 14:15:58 +0000

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -7,18 +7,17 @@ resource "aws_db_instance" "my-test-sql" {
   storage_type            = "gp2"
   allocated_storage       = 20
   name                    = "MyRDS"
-  username                = "admin"
-  password                = "admin123"
+  username                = "default" //"admin"
+  password                = "default" //"admin123"
   apply_immediately       = "true"
   backup_retention_period = 10
-  //backup_window           = "09:46-10:16"
   db_subnet_group_name    = aws_db_subnet_group.my-rds-db-subnet.name
-  vpc_security_group_ids  = [aws_security_group.DB-sg.id] //aws_security_group.my-rds-sg.id //["${aws_security_group.my-rds-sg.id}"]
+  vpc_security_group_ids  = [aws_security_group.DB-sg.id] 
 }
 
 resource "aws_db_subnet_group" "my-rds-db-subnet" {
   name       = "my-rds-db-subnet"
-  subnet_ids = [var.rds_subnet1, var.rds_subnet2] //["${var.rds_subnet1}", "${var.rds_subnet2}"]
+  subnet_ids = [var.rds_subnet1, var.rds_subnet2] 
 
   tags = {
     Name = "My DB subnet group"
@@ -27,7 +26,7 @@ resource "aws_db_subnet_group" "my-rds-db-subnet" {
 /*
 resource "aws_security_group" "my-rds-sg" {
   name   = "my-rds-sg"
-  vpc_id = var.vpc_id //"${var.vpc_id}"
+  vpc_id = var.vpc_id 
 
 }
 
@@ -37,7 +36,7 @@ resource "aws_security_group_rule" "my-rds-sg-rule" {
   security_group_id = aws_security_group.my-rds-sg.id
   to_port           = 3306
   type              = "ingress"
-  source_security_group_id   = var.security_groups //["${var.security_groups}"]
+  source_security_group_id   = var.security_groups 
 }
 
 resource "aws_security_group_rule" "outbound_rule" {
@@ -52,14 +51,14 @@ resource "aws_security_group_rule" "outbound_rule" {
 resource "aws_security_group" "DB-sg" {
   name        = "Database SG"
   description = "Allow  traffic from public subnet instances"
-  vpc_id = var.vpc_id //vpc_id      = aws_vpc.main.id
+  vpc_id = var.vpc_id 
 
   ingress {
     description = "MySQL DB access port"
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    security_groups   = [var.security_groups] //cidr_blocks = ["0.0.0.0/0"]
+    security_groups   = [var.security_groups] 
   }
 
   ingress {
@@ -67,7 +66,7 @@ resource "aws_security_group" "DB-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    security_groups   = [var.security_groups] //cidr_blocks = ["0.0.0.0/0"]
+    security_groups   = [var.security_groups] 
   }
 
   egress {
