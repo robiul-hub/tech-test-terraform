6e7fa0e8f697b9b48d5dcc89f57999a8fae3043a
robiul-hub <75095486+robiul-hub@users.noreply.github.com>
2021-01-29 00:00:35 +0000

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -22,31 +22,7 @@ resource "aws_db_subnet_group" "my-rds-db-subnet" {
     Name = "My DB subnet group"
   }
 }
-/*
-resource "aws_security_group" "my-rds-sg" {
-  name   = "my-rds-sg"
-  vpc_id = var.vpc_id 
 
-}
-
-resource "aws_security_group_rule" "my-rds-sg-rule" {
-  from_port         = 3306
-  protocol          = "tcp"
-  security_group_id = aws_security_group.my-rds-sg.id
-  to_port           = 3306
-  type              = "ingress"
-  source_security_group_id   = var.security_groups 
-}
-
-resource "aws_security_group_rule" "outbound_rule" {
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.my-rds-sg.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-*/
 resource "aws_security_group" "DB-sg" {
   name        = "Database SG"
   description = "Allow  traffic from public subnet instances"
