4918d2b4481847402ddfdb5bd430952c240dbf3f
robiul-hub <75095486+robiul-hub@users.noreply.github.com>
2021-01-29 10:58:03 +0000

    Update main.tf

--- asg/main.tf
+++ asg/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "asg-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] // It shoud be accessible from company IP add range
   }
 
   egress {
